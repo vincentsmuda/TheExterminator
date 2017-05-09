31a5a0e848449aa8e5c564b3ad162aec059228be
Vincent Smuda <vincent@vincentsmuda.com>
2017-05-09 17:11:11 -0400

    recompiled dist

--- dist/exterminator.js
+++ dist/exterminator.js
@@ -118,15 +118,15 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 var _templateObject = _taggedTemplateLiteral([','], [',']);
 
-var _Detective = __webpack_require__(4);
+var _Detective = __webpack_require__(5);
 
 var _Detective2 = _interopRequireDefault(_Detective);
 
-var _html2canvas = __webpack_require__(8);
+var _html2canvas = __webpack_require__(9);
 
 var _html2canvas2 = _interopRequireDefault(_html2canvas);
 
-var _fields = __webpack_require__(5);
+var _fields = __webpack_require__(6);
 
 var _fields2 = _interopRequireDefault(_fields);
 
@@ -922,10 +922,10 @@ module.exports = function () {
 // style-loader: Adds some css to the DOM by adding a <style> tag
 
 // load the styles
-var content = __webpack_require__(6);
+var content = __webpack_require__(7);
 if(typeof content === 'string') content = [[module.i, content, '']];
 // add the styles to the DOM
-var update = __webpack_require__(10)(content, {});
+var update = __webpack_require__(11)(content, {});
 if(content.locals) module.exports = content.locals;
 // Hot Module Replacement
 if(false) {
@@ -973,17 +973,215 @@ new _CallTheExterminator2.default(settings);
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ *  A helper class to check download speeds
+ */
+
+module.exports = function () {
+
+  /**
+   *  Construct the class with arguments
+   *  that override defaults
+   */
+  function Bandwidth(args) {
+    _classCallCheck(this, Bandwidth);
+
+    // Set the defaults
+    Object.assign(this, {
+      'image': 'https://images.unsplash.com/reserve/E5CwLOxQSFimIXJurfpq_IMG_6424%20(1).jpg',
+      'download_size': 2261022,
+      'tests_to_run': 10,
+      'test_offset': 1000,
+      'autostart': false
+    }, args);
+
+    // how to devide the bits
+    this.per_k = 1000;
+
+    // Set how accurate we need it to be
+    this.decimal_places = 2;
+
+    // Array to store the tests
+    this.tests = [];
+
+    // To store the results
+    this.results = {};
+
+    // Start the tests
+    if (this.autostart) this.startTests();
+  }
+
+  /**
+   *  Starts our tests with the givven paramaters
+   */
+
+
+  _createClass(Bandwidth, [{
+    key: 'startTests',
+    value: function startTests() {
+      var _this = this;
+
+      // Wait for the window to load before
+      // starting our tests
+      window.addEventListener('load', function () {
+        _this.test();
+      });
+    }
+
+    /**
+     *  We get our test results from here
+     */
+
+  }, {
+    key: 'getResults',
+    value: function getResults() {
+      return this.results;
+    }
+
+    /**
+     *  Processes the results of all of the tests
+     */
+
+  }, {
+    key: 'processResults',
+    value: function processResults() {
+
+      // Reset the aggregate results
+      var duration = 0,
+          speed_bps = 0,
+          speed_kbps = 0,
+          speed_mbps = 0,
+          fastest_mbps = 1e20,
+          slowest_mbps = 0,
+          fastest_duration = 1e20,
+          slowest_duration = 0,
+          test_count = this.tests.length;
+
+      // Add up all of the durations
+      for (var i = 0; i < test_count; i++) {
+
+        // Add the duration
+        duration += this.tests[i].duration;
+
+        // Add the bits per second
+        speed_bps += +this.tests[i].speed_bps;
+
+        // Set up the fastest MBps
+        if (this.tests[i].speed_mbps < fastest_mbps) fastest_mbps = this.tests[i].speed_mbps;
+
+        // Set up the slowest MBps
+        if (this.tests[i].speed_mbps > slowest_mbps) slowest_mbps = this.tests[i].speed_mbps;
+
+        // Set up the fastest duration
+        if (this.tests[i].duration < fastest_duration) fastest_duration = this.tests[i].duration;
+
+        // Set up the slowest duration
+        if (this.tests[i].duration > slowest_duration) slowest_duration = this.tests[i].duration;
+      }
+
+      // Store the aggregate results
+      this.results = {
+        duration: +(duration / test_count).toFixed(this.decimal_places),
+        speed_bps: +(speed_bps / test_count).toFixed(this.decimal_places),
+        speed_kbps: +(speed_bps / this.per_k / test_count).toFixed(this.decimal_places),
+        speed_mbps: +(speed_bps / this.per_k / this.per_k / test_count).toFixed(this.decimal_places),
+        fastest_mbps: +(+fastest_mbps).toFixed(this.decimal_places),
+        slowest_mbps: +(+slowest_mbps).toFixed(this.decimal_places),
+        fastest_duration: +fastest_duration.toFixed(this.decimal_places),
+        slowest_duration: +slowest_duration.toFixed(this.decimal_places)
+      };
+
+      // Return the aggregate results
+      return this.results;
+    }
+
+    /**
+     *  Test download speed
+     */
+
+  }, {
+    key: 'test',
+    value: function test() {
+      var _this2 = this;
+
+      // Init our vars
+      var startTime = void 0,
+          endTime = void 0,
+          cacheBuster = void 0,
+          download = new Image();
+
+      // Set a watcher that will watch for
+      // the end of the image loading
+      download.addEventListener('load', function () {
+
+        // Sets the endtime
+        endTime = new Date().getTime();
+
+        // So something here
+        var duration = (endTime - startTime) / 1000,
+            bitsLoaded = _this2.download_size * 8,
+            speed_bps = (bitsLoaded / duration).toFixed(_this2.decimal_places),
+            speed_kbps = (speed_bps / _this2.per_k).toFixed(_this2.decimal_places),
+            speed_mbps = (speed_kbps / _this2.per_k).toFixed(_this2.decimal_places);
+
+        // Add the results to the tests
+        _this2.tests.push({
+          duration: duration,
+          speed_bps: speed_bps,
+          speed_kbps: speed_kbps,
+          speed_mbps: speed_mbps
+        });
+
+        // Once we've reached the last test we will
+        // process the results
+        _this2.processResults();
+
+        // If we haven't reached the tests to run
+        // just go on and run another!
+        if (_this2.tests.length < _this2.tests_to_run) _this2.test();
+      });
+
+      // Handle the error if one should occur
+      download.addEventListener('error', function (err, msg) {
+        console.log("Invalid image, or error downloading");
+      });
+
+      // Set the start time to measure against
+      startTime = new Date().getTime();
+
+      // Set the cache busting based on the start time
+      cacheBuster = "?nnn=" + startTime;
+
+      // Set the image src w/ cache busting
+      download.src = this.image + cacheBuster;
+    }
+  }]);
+
+  return Bandwidth;
+}();
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); // Grab Platform.js for browser info
 
 
 // Grab the bandwidth tester
 
 
-var _platform = __webpack_require__(9);
+var _platform = __webpack_require__(10);
 
 var _platform2 = _interopRequireDefault(_platform);
 
-var _Bandwidth = __webpack_require__(13);
+var _Bandwidth = __webpack_require__(4);
 
 var _Bandwidth2 = _interopRequireDefault(_Bandwidth);
 
@@ -1310,7 +1508,7 @@ module.exports = function () {
 }();
 
 /***/ }),
-/* 5 */
+/* 6 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -1343,10 +1541,10 @@ module.exports = [{
 }];
 
 /***/ }),
-/* 6 */
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
-exports = module.exports = __webpack_require__(7)(undefined);
+exports = module.exports = __webpack_require__(8)(undefined);
 // imports
 
 
@@ -1357,7 +1555,7 @@ exports.push([module.i, "/**\n *  Base class of the component\n *  You can chang
 
 
 /***/ }),
-/* 7 */
+/* 8 */
 /***/ (function(module, exports) {
 
 /*
@@ -1439,7 +1637,7 @@ function toComment(sourceMap) {
 
 
 /***/ }),
-/* 8 */
+/* 9 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(global) {var require;var require;/*
@@ -4990,7 +5188,7 @@ module.exports = XHR;
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
 /***/ }),
-/* 9 */
+/* 10 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /* WEBPACK VAR INJECTION */(function(module, global) {var __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -6212,10 +6410,10 @@ module.exports = XHR;
   }
 }.call(this));
 
-/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(12)(module), __webpack_require__(0)))
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(13)(module), __webpack_require__(0)))
 
 /***/ }),
-/* 10 */
+/* 11 */
 /***/ (function(module, exports, __webpack_require__) {
 
 /*
@@ -6252,7 +6450,7 @@ var stylesInDom = {},
 	singletonElement = null,
 	singletonCounter = 0,
 	styleElementsInsertedAtTop = [],
-	fixUrls = __webpack_require__(11);
+	fixUrls = __webpack_require__(12);
 
 module.exports = function(list, options) {
 	if(typeof DEBUG !== "undefined" && DEBUG) {
@@ -6511,7 +6709,7 @@ function updateLink(linkElement, options, obj) {
 
 
 /***/ }),
-/* 11 */
+/* 12 */
 /***/ (function(module, exports) {
 
 
@@ -6606,7 +6804,7 @@ module.exports = function (css) {
 
 
 /***/ }),
-/* 12 */
+/* 13 */
 /***/ (function(module, exports) {
 
 module.exports = function(module) {
@@ -6632,204 +6830,6 @@ module.exports = function(module) {
 	return module;
 };
 
-
-/***/ }),
-/* 13 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-/**
- *  A helper class to check download speeds
- */
-
-module.exports = function () {
-
-  /**
-   *  Construct the class with arguments
-   *  that override defaults
-   */
-  function Bandwidth(args) {
-    _classCallCheck(this, Bandwidth);
-
-    // Set the defaults
-    Object.assign(this, {
-      'image': 'https://images.unsplash.com/reserve/E5CwLOxQSFimIXJurfpq_IMG_6424%20(1).jpg',
-      'download_size': 2261022,
-      'tests_to_run': 10,
-      'test_offset': 1000,
-      'autostart': false
-    }, args);
-
-    // how to devide the bits
-    this.per_k = 1000;
-
-    // Set how accurate we need it to be
-    this.decimal_places = 2;
-
-    // Array to store the tests
-    this.tests = [];
-
-    // To store the results
-    this.results = {};
-
-    // Start the tests
-    if (this.autostart) this.startTests();
-  }
-
-  /**
-   *  Starts our tests with the givven paramaters
-   */
-
-
-  _createClass(Bandwidth, [{
-    key: 'startTests',
-    value: function startTests() {
-      var _this = this;
-
-      // Wait for the window to load before
-      // starting our tests
-      window.addEventListener('load', function () {
-        _this.test();
-      });
-    }
-
-    /**
-     *  We get our test results from here
-     */
-
-  }, {
-    key: 'getResults',
-    value: function getResults() {
-      return this.results;
-    }
-
-    /**
-     *  Processes the results of all of the tests
-     */
-
-  }, {
-    key: 'processResults',
-    value: function processResults() {
-
-      // Reset the aggregate results
-      var duration = 0,
-          speed_bps = 0,
-          speed_kbps = 0,
-          speed_mbps = 0,
-          fastest_mbps = 1e20,
-          slowest_mbps = 0,
-          fastest_duration = 1e20,
-          slowest_duration = 0,
-          test_count = this.tests.length;
-
-      // Add up all of the durations
-      for (var i = 0; i < test_count; i++) {
-
-        // Add the duration
-        duration += this.tests[i].duration;
-
-        // Add the bits per second
-        speed_bps += +this.tests[i].speed_bps;
-
-        // Set up the fastest MBps
-        if (this.tests[i].speed_mbps < fastest_mbps) fastest_mbps = this.tests[i].speed_mbps;
-
-        // Set up the slowest MBps
-        if (this.tests[i].speed_mbps > slowest_mbps) slowest_mbps = this.tests[i].speed_mbps;
-
-        // Set up the fastest duration
-        if (this.tests[i].duration < fastest_duration) fastest_duration = this.tests[i].duration;
-
-        // Set up the slowest duration
-        if (this.tests[i].duration > slowest_duration) slowest_duration = this.tests[i].duration;
-      }
-
-      // Store the aggregate results
-      this.results = {
-        duration: +(duration / test_count).toFixed(this.decimal_places),
-        speed_bps: +(speed_bps / test_count).toFixed(this.decimal_places),
-        speed_kbps: +(speed_bps / this.per_k / test_count).toFixed(this.decimal_places),
-        speed_mbps: +(speed_bps / this.per_k / this.per_k / test_count).toFixed(this.decimal_places),
-        fastest_mbps: +(+fastest_mbps).toFixed(this.decimal_places),
-        slowest_mbps: +(+slowest_mbps).toFixed(this.decimal_places),
-        fastest_duration: +fastest_duration.toFixed(this.decimal_places),
-        slowest_duration: +slowest_duration.toFixed(this.decimal_places)
-      };
-
-      // Return the aggregate results
-      return this.results;
-    }
-
-    /**
-     *  Test download speed
-     */
-
-  }, {
-    key: 'test',
-    value: function test() {
-      var _this2 = this;
-
-      // Init our vars
-      var startTime = void 0,
-          endTime = void 0,
-          cacheBuster = void 0,
-          download = new Image();
-
-      // Set a watcher that will watch for
-      // the end of the image loading
-      download.addEventListener('load', function () {
-
-        // Sets the endtime
-        endTime = new Date().getTime();
-
-        // So something here
-        var duration = (endTime - startTime) / 1000,
-            bitsLoaded = _this2.download_size * 8,
-            speed_bps = (bitsLoaded / duration).toFixed(_this2.decimal_places),
-            speed_kbps = (speed_bps / _this2.per_k).toFixed(_this2.decimal_places),
-            speed_mbps = (speed_kbps / _this2.per_k).toFixed(_this2.decimal_places);
-
-        // Add the results to the tests
-        _this2.tests.push({
-          duration: duration,
-          speed_bps: speed_bps,
-          speed_kbps: speed_kbps,
-          speed_mbps: speed_mbps
-        });
-
-        // Once we've reached the last test we will
-        // process the results
-        _this2.processResults();
-
-        // If we haven't reached the tests to run
-        // just go on and run another!
-        if (_this2.tests.length < _this2.tests_to_run) _this2.test();
-      });
-
-      // Handle the error if one should occur
-      download.addEventListener('error', function (err, msg) {
-        console.log("Invalid image, or error downloading");
-      });
-
-      // Set the start time to measure against
-      startTime = new Date().getTime();
-
-      // Set the cache busting based on the start time
-      cacheBuster = "?nnn=" + startTime;
-
-      // Set the image src w/ cache busting
-      download.src = this.image + cacheBuster;
-    }
-  }]);
-
-  return Bandwidth;
-}();
 
 /***/ })
 /******/ ]);
